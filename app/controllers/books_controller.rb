f4ee5726b84901b31502bbb69bfa47774be62520
tomohito-tajima <tomo322017@gmail.com>
2021-10-20 09:25:35 +0000

    add レイアウト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,34 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
   end
-  
+
   def index
+    @book = Book.new
+    @user = current_user
+    @books = Book.all
   end
 
   def show
   end
-  
+
   def edit
   end
 
   def destroy
   end
+
+   private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
+
+  get 'homes/about', to: 'homes#about'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
+  resources :books
 
-  resources :users, only: [:index, :edit, :update]
+  resources :users, only: [:show, :index, :edit, :update]
 end
