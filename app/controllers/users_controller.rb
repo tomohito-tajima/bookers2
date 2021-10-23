5ad4ce8e6cd5e83121784c73459e47db1bc446d1
tomohito-tajima <tomo322017@gmail.com>
2021-10-23 10:19:43 +0000

    Initial Commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = current_user
+    @book = Book.new
+    @books = Book.find(params[:id])
+  end
+
   def index
-    @users=User.all
+    @book = Book.new
+    @user = current_user
+    @users = User.all
   end
 
   def edit
