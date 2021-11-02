1de9d0aa7ed824b2a561bcad0a578f5e1ae26518
tomohito-tajima <tomo322017@gmail.com>
2021-11-02 10:02:53 +0000

    Next フォロー機能view

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,28 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  #ファロー・フォロワー機能のアソシエーション
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+  #ここまで
+  #ファロー・フォロワー機能のメソッド
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
+  #ここまで
+
+
   attachment :profile_image
 
   validates :name, length: {minimum: 2, maximum: 20 }, uniqueness: true
