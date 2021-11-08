e07b6263afdec2328b1cfb26ae1f4d1ce3a72f6a
tomohito-tajima <tomo322017@gmail.com>
2021-11-08 07:53:45 +0000

    Finish 非同期通信

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,17 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    @book_comment.save
+    #redirect_to book_path(book) 非同期通信を行うため削除
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
     BookComment.find_by(id: params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    #redirect_to book_path(params[:book_id]) 非同期通信を行うため削除
   end
 
   private
