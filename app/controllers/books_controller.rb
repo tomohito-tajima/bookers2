a2d512d7f2115deaa29559ad057b2993b1228560
tomohito-tajima <tomo322017@gmail.com>
2021-11-10 05:22:58 +0000

    fix コメント非同期機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,7 +27,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    @info = Book.new
+    @new_book = Book.new
     @book_comment = BookComment.new
   end
 
